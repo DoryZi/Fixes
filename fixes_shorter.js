d60949dfdd5f97782246931d0638e5693406e1f7
Dory Zidon <dory.zidon@gmail.com>
2015-10-16 14:01:01 -0700

    fixed ref count error, fixed allowing double close, added anonymous function init

--- fixes_shorter.js
+++ fixes_shorter.js
@@ -13,58 +13,61 @@
 if (typeof NAMESPACE === 'undefined' || NAMESPACE === null) {
     NAMESPACE = {};
 
-	// 1) Must move the _all_ids out of the internal function (resource).
-	// before it would create a new array each time, and not maintain the resources
-	// as the function lookup or create implies, it would create a new resource each time.
-	// also the close seems to delete only the element in the key position.
-	// 2) furthermore this should not be an array but an associative array, which is an object and not 
-	// an array. 
-	var _all_ids = {}; //new Array();
+	(function() {
+		// 1) Must move the _all_ids out of the internal function (resource).
+		// before it would create a new array each time, and not maintain the resources
+		// as the function lookup or create implies, it would create a new resource each time.
+		// also the close seems to delete only the element in the key position.
+		// 2) furthermore this should not be an array but an associative array, which is an object and not 
+		// an array. 
+		var _all_ids = {}; //new Array();
 
 
-   // Private methods
-	var _lookupOrCreateExpensiveResourceById = function(id) {
-		_expensive_resource = _all_ids[id].res;
-		_all_ids[id].count += 1;
-		
-		if (_expensive_resource == null) {
-			// Just pretend for the sake of this example
-			_expensive_resource = {
-				value: "I'm a very expensive resource associated with ID " + id
-			};
+	   // Private methods
+		var _lookupOrCreateExpensiveResourceById = function(id) {
+			var _expensive_resource = null;
+			if (_all_ids[id]) {
+			 _expensive_resource = _all_ids[id].res;
+			_all_ids[id].count += 1;
+			} else {
+				// Just pretend for the sake of this example
+				_expensive_resource = {
+					value: "I'm a very expensive resource associated with ID " + id
+				};
 
-			_all_ids[id] = { res : _expensive_resource, count : 1};
+				_all_ids[id] = { res : _expensive_resource, count : 1};
+			}
+			
+			return _expensive_resource;
 		}
-		
-		return _expensive_resource;
-	}
- 
+	 
 
-     
-    // Creates an object that allocates a new or references an
-    // existing very expensive resource associated with `id`
-    var resource = function (id) {
-	    // Private data
-        var _closed = false;
-        var _id = id;
-        var _expensive_resource = _lookupOrCreateExpensiveResourceById(id);
-		
-        // Public data
-        return {
-		  getExpensiveResource : function () {
-            return (_closed) ? null : _expensive_resource;
-		  },
-		  getId : function () {
-            return _id;
-          },
-		  close : function () {
-			_all_ids[_id].count -= 1;
-			if (_all_ids[_id].count < 1) delete _all_ids[_id];
-            _closed = true;
-          }
-        };
-    };
+		 
+		// Creates an object that allocates a new or references an
+		// existing very expensive resource associated with `id`
+		var resource = function (id) {
+			// Private data
+			var _closed = false;
+			var _id = id;
+			var _expensive_resource = _lookupOrCreateExpensiveResourceById(id);
+			
+			// Public data
+			return {
+			  getExpensiveResource : function () {
+				return (_closed) ? null : _expensive_resource;
+			  },
+			  getId : function () {
+				return _id;
+			  },
+			  close : function () {
+			    if (_closed) return;
+				_all_ids[_id].count -= 1;
+				if (_all_ids[_id].count < 1) delete _all_ids[_id];
+				_closed = true;
+			  }
+			};
+		};
 
-    NAMESPACE.resource = resource;
+		NAMESPACE.resource = resource;
+	})();
 }
-
